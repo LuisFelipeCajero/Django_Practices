f092839ba4f4fb7e700d0c6366d36ca7c8507541
Luis Felipe Cajero <luis98_cajero@hotmail.com>
2023-02-07 16:14:18 -0600

    Add views and conect with urls in polls app

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world")
+    return HttpResponse("Estas en la página principal de Premios Platzi App")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta número{question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta número{question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta número {question_id}")
+
+
