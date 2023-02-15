64a01eff91ea30aa515f3f16f3981fd8b5d41755
Luis Felipe Cajero <luis98_cajero@hotmail.com>
2023-02-15 15:40:38 -0600

    Add 404 function, connect url with polls

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -10,7 +10,12 @@ def index(request):
     })
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta número{question_id}")
+#    return HttpResponse(f"Estas viendo la pregunta número{question_id}")
+     question = get_list_or_404(Question , pk=question_id)
+     return render(request, "Polls/detail.html", {
+        "question": question
+     })
+
 
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta número{question_id}")
